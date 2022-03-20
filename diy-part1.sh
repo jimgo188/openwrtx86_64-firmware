65353ba8d937486b1f170cf2e0de380a3128ff88
jimgo188 <50714117+jimgo188@users.noreply.github.com>
2022-03-20 21:01:43 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,4 +16,3 @@
 # Add a feed source
 echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-# echo 'src-git small8 https://github.com/Lenyu2020/small-package' >>feeds.conf.default
