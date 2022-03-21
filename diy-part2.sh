b1030ec4ffb83c8feec11af96c78c7823bdc2908
jimgo188 <50714117+jimgo188@users.noreply.github.com>
2022-03-21 15:29:27 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -14,15 +14,16 @@
 # sed -i 's/192.168.1.1/192.168.50.1/g' package/base-files/files/bin/config_generate
 # Modify default Kernel
 sed -i 's/KERNEL_PATCHVER:=5.15/KERNEL_PATCHVER:=5.4/g' target/linux/x86/Makefile
-#delete netdate
-# rm -rf feeds/luci/applications/luci-app-netdata
 #delete wrtbwmon
 # rm -rf feeds/luci/applications/luci-app-wrtbwmon
 # add chajian
 # git clone https://github.com/iwrt/luci-app-ikoolproxy.git package/luci-app-ikoolproxy
 # git clone https://github.com/tty228/luci-app-serverchan.git package/luci-app-serverchan
-# git clone https://github.com/jimgo188/op-package package/op-package
+git clone https://github.com/jimgo188/op-package package/op-package
 git clone -b files https://github.com/jimgo188/op-package files
 # git clone https://github.com/sirpdboy/sirpdboy-package.git package/sirpdboy-package
 git clone https://github.com/kenzok8/small-package package/small-package
 # git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon-18.06
+#delete netdate
+rm -rf feeds/luci/applications/luci-app-netdata
+rm -rf package/small-package/luci-app-netdata
