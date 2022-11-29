bfc95b65286107d3e273705194953f360ea2ebc1
jimgo188 <50714117+jimgo188@users.noreply.github.com>
2022-11-29 09:19:52 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -29,5 +29,6 @@ git clone -b files https://github.com/jimgo188/op-package files
 git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon-18.06
 git clone https://github.com/jerrykuku/luci-app-argon-config.git package/luci-app-argon-config
 git clone https://github.com/eerrjjkk/luci-app-vnstat2.git package/luci-app-vnstat2
-# git clone https://github.com/sbwml/luci-app-mosdns package/mosdns
-# git clone https://github.com/sbwml/v2ray-geodata package/geodata
+rm -rf feeds/luci/applications/luci-app-mosdns
+rm -rf feeds/packages/net/mosdns
+git clone https://github.com/sbwml/luci-app-mosdns package/mosdns
